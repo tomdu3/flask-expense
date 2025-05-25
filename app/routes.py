87d1e531f8580c266d2d217e12dca4b63b3b67b2
Tomislav Dukez <tomdu3@ymail.com>
2025-05-25 10:51:24 +0100

    add base and refactor template

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
 from app import app
 from flask import render_template
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+@app.route("/layout")
+def base():
+    return render_template("base.html")
