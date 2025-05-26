f055ebf238f66a1d3390b60c6166d5c8b89a61b7
Tomislav Dukez <tomdu3@ymail.com>
2025-05-26 23:03:01 +0100

    add db config

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expensesDB.db'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or ''
+
+db = SQLAlchemy(app)
 
 from app import routes
 
