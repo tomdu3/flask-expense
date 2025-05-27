b44dbf59c352853ecc3568af96a0e4ba982604b2
Tomislav Dukez <tomdu3@ymail.com>
2025-05-27 08:21:54 +0100

    add add expense route

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,28 @@
-from app import app
-from flask import render_template
-
+from app import app, db
+from flask import render_template, flash, redirect, url_for
+from app.forms import UserInputForm
+from app.models import IncomeExpenses
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/layout")
-def base():
-    return render_template("base.html", title="Layout")
+@app.route("/add", methods=["GET", "POST"])
+def add_expense():
+    form = UserInputForm()
+    if form.validate_on_submit():
+        entry = IncomeExpenses(
+            type=form.type.data,
+            amount=form.amount.data,
+            category=form.category.data,
+            description=form.description.data,
+        )
+        db.session.add(entry)
+        db.session.commit()
+        flash("Entry added successfully!", "success")
+        return redirect(url_for("index"))
+    return render_template("add.html", title="Add Expense/Income Entry", form=form)
 
 
 @app.route("/flowbite-test")
