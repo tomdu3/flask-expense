996f6f19a437dec1ebd45aadd4ddd947eb41c804
Tomislav Dukez <tomdu3@ymail.com>
2025-07-17 08:18:28 +0100

    add whitenoise library for static files

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from whitenoise import WhiteNoise
 
 app = Flask(__name__)
+app = WhiteNoise(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expensesDB.db'
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or ''
