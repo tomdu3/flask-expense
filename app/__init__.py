138c52b9138e6d8ff6e50fd9e0536b7cad7a9ddc
Tomislav Dukez <tomdu3@ymail.com>
2025-08-22 01:54:27 +0100

    refactor: alternative static files loading

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,13 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from whitenoise import WhiteNoise
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Initialize Flask application
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI') or 'sqlite:///expensesDB.db'
 print("Using database URI:", app.config['SQLALCHEMY_DATABASE_URI'])
@@ -17,6 +16,3 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or ''
 db = SQLAlchemy(app)
 
 from app import routes
-
-app.wsgi_app = WhiteNoise(app.wsgi_app, root=os.path.join(os.path.dirname(__file__), "static"))
-
