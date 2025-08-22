96a63ac4c10089e16c653ebaaf1d09688b257915
Tomislav Dukez <tomdu3@ymail.com>
2025-08-22 02:11:53 +0100

    refactor whitenoise

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,15 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from whitenoise import WhiteNoise
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Initialize Flask application
 app = Flask(__name__, static_folder='static', static_url_path='/static')
+static_path = os.path.join(os.path.dirname(__file__), 'static')
+app.wsgi_app = WhiteNoise(app.wsgi_app, root=static_path)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI') or 'sqlite:///expensesDB.db'
 print("Using database URI:", app.config['SQLALCHEMY_DATABASE_URI'])
